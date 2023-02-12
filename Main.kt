15ce690701d46d19d2de2b4a0813ee26b30ebafc
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2023-02-12 16:32:32 +0200

    Stage 2

--- Main.kt
+++ Main.kt
@@ -1,4 +1,4 @@
-fun main() {
+fun cinemaHall() {
     println("""
         Cinema:
           1 2 3 4 5 6 7 8
@@ -10,4 +10,26 @@ fun main() {
         6 S S S S S S S S
         7 S S S S S S S S
     """.trimIndent())
+}
+
+const val tenDollars = 10
+const val eightDollars = 8
+
+fun main() {
+    cinemaHall()
+
+    println("Enter the number of rows:")
+    val rows = readln().toInt()
+
+    println("Enter the number of seats in each row:")
+    val cols = readln().toInt()
+
+    println("Total income:")
+    print(when {
+        rows * cols <= 60 -> rows * cols * tenDollars
+        else -> {
+            val hallHalf = rows / 2
+            (hallHalf * cols * tenDollars) + (rows - hallHalf) * cols * eightDollars
+        }
+    })
 }

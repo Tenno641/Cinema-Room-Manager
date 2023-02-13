347de26ae03f0dd6518f2a36229ce137179aa869
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2023-02-13 03:11:40 +0200

    Stage 4

--- Main.kt
+++ Main.kt
@@ -1,21 +1,4 @@
-fun cinemaHall(rows: Int, cols: Int, ticketR: Int = 0, ticketC: Int = 0) {
-    println("Cinema:")
-    print(" ")
-    for (i in 1..cols) {
-        print(" $i")
-    }
-    println()
-    for (i in 1..rows) {
-        print("$i")
-        for (j in 1..cols) {
-            when {
-                i == ticketR && j == ticketC -> print(" B")
-                else -> print(" S")
-            }
-        }
-        println()
-    }
-}
+val board = mutableListOf<MutableList<Char>>()
 
 const val tenDollars = 10
 const val eightDollars = 8
@@ -28,20 +11,40 @@ fun main() {
     println("Enter the number of seats in each row:")
     val cols = readln().toInt()
 
-    cinemaHall(rows, cols)
+    for (i in 1..cols) { board.add(MutableList(cols) {'S'}) }
 
-    println("Enter a row number:")
-    val ticketRow = readln().toInt()
+    do {
+        println("1. Show the seats\n2. Buy a ticket\n0. Exit")
+        val choice = readln().first()
 
-    println("Enter a seat number in that row:")
-    val ticketCol = readln().toInt()
+        when(choice) {
+            '1' -> {
+                print("Cinema:\n ")
+                for (i in 1..cols) { print(" $i") }
 
-    print("Ticket price: $")
-    println(when {
-        rows * cols <= 60 -> tenDollars
-        else -> {
-            if(ticketRow <= rows / 2) tenDollars else eightDollars
+                println()
+
+                for (i in 1..rows) { print("$i ${board[i].joinToString(" ")}"); println() }
+            }
+
+            '2' -> {
+                println("Enter a row number:")
+                val ticketRow = readln().toInt()
+
+                println("Enter a seat number in that row:")
+                val ticketCol = readln().toInt()
+
+                print("Ticket price: $")
+                println(when {
+                    rows * cols <= 60 -> tenDollars
+                    else -> {
+                        if(ticketRow <= rows / 2) tenDollars else eightDollars
+                    }
+                })
+
+                board[ticketRow][ticketCol - 1] = 'B'
+
+            }
         }
-    })
-    cinemaHall(rows, cols, ticketRow, ticketCol)
+    } while (choice != '0')
 }

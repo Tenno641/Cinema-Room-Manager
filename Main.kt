463f7e8fb5cdcd7a3b17064af5072b5d87f8772d
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2023-02-15 04:34:53 +0200

    Stage 5 Finally!

--- Main.kt
+++ Main.kt
@@ -1,6 +1,10 @@
 const val tenDollars = 10
 const val eightDollars = 8
 
+var purchasedTickets = 0
+var currentIncome = 0
+var totalIncome = 0
+
 fun main() {
 
     println("Enter the number of rows:")
@@ -9,10 +13,12 @@ fun main() {
     println("Enter the number of seats in each row:")
     val cols = readln().toInt()
 
+    val seats = rows * cols
+
     val board = MutableList(rows + 1) {MutableList(cols) {'S'} }
 
     do {
-        println("1. Show the seats\n2. Buy a ticket\n0. Exit")
+        println("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit")
         val choice = readln().first()
 
         when(choice) {
@@ -26,23 +32,57 @@ fun main() {
             }
 
             '2' -> {
-                println("Enter a row number:")
-                val ticketRow = readln().toInt()
+                 while (true) {
+                     println("Enter a row number:")
+                     val ticketRow = readln().toInt()
 
-                println("Enter a seat number in that row:")
-                val ticketCol = readln().toInt()
+                     println("Enter a seat number in that row:")
+                     val ticketCol = readln().toInt()
 
-                print("Ticket price: $")
-                println(when {
-                    rows * cols <= 60 -> tenDollars
-                    else -> {
-                        if(ticketRow <= rows / 2) tenDollars else eightDollars
-                    }
-                })
+                     if (ticketRow > rows || ticketCol > cols) {
+                         println("Wrong input!")
+                         continue
+                     }
 
-                board[ticketRow][ticketCol - 1] = 'B'
+                     if (board[ticketRow][ticketCol - 1] == 'B') {
+                         println("That ticket has already been purchased!")
+                         continue
+                     }
 
+                     print("Ticket price: $")
+                     currentIncome += when {
+                         seats <= 60 -> {
+                             println(tenDollars)
+                             tenDollars
+                         }
+
+                         else -> {
+                             println(if (ticketRow <= rows / 2) tenDollars else eightDollars)
+                             if (ticketRow <= rows / 2) tenDollars else eightDollars
+                         }
+                     }
+
+                     board[ticketRow][ticketCol - 1] = 'B'
+                     purchasedTickets++
+                     break
+
+                 }
+            }
+
+            '3' -> {
+                val percentage = (purchasedTickets.toDouble() / (rows * cols)) * 100
+
+                totalIncome = if(seats <= 60) seats * tenDollars
+                else ((rows / 2) * cols * tenDollars) + ((rows - (rows / 2)) * cols * eightDollars)
+
+                println("""
+                Number of purchased tickets: $purchasedTickets
+                Percentage: ${"%.2f".format(percentage)}%
+                Current income: $${currentIncome}
+                Total income: $$totalIncome
+            """.trimIndent())
             }
         }
+
     } while (choice != '0')
 }
